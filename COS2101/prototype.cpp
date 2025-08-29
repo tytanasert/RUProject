3827fa572b84b8443cff7cc62dca4b9a9613961e
tytanasert <tytanasert@gmail.com>
2025-08-29 14:14:27 +0700

    Create prototype.cpp

---
+++ COS2101/prototype.cpp
@@ -0,0 +1,34 @@
+
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+#include<iostream>
+using namespace std;
+void print_Jennifer (  );
+// Function to print program title to screen.
+
+
+void print_title (  )
+ {
+  cout << "Tennis Tournament Scheduler Program\n";
+  print_Jennifer (  );
+ }
+void print_Jennifer (  )
+ {
+  cout << "By Jennifer Baker\n";
+ }
+
+int main (  )
+ {
+     print_title(  );      // call to print_title
+     print_title(  );      // call to print_title
+     
+     print_Jennifer (  );
+      
+      
+     return 0;
+ } // end of main function
+
+
+
+
+
+
